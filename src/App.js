4409f48c1278223374e66bd7acc52316d6d0b4e8
rikvanvelzen <rikvanvelzen@yahoo.com>
2020-09-09 16:21:59 +0200

    react router installed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM naming convention used throughout the app
-    <div className="app">
-      <Header />
-      <Home />
-      left off at 56:19 building the 2nd page with react router
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+          {/* left off at 1:00:55 building the 2nd page with react router */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
